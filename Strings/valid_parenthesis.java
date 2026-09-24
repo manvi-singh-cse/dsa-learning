b6ee0c2b3fa9960aeaa321d933fe9336bd1a212a
Manvi Singh <singhmanvi2202@gmail.com>
2026-09-24 23:17:54 +0530

    Add program to check for valid parenthesis

---
+++ Strings/valid_parenthesis.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class valid_parenthesis {
+    public static boolean isValidParenthesis(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "({[]})";
+        System.out.println(isValidParenthesis(s));
+
+        s = "({[})";
+        System.out.println(isValidParenthesis(s));
+    }
+}
